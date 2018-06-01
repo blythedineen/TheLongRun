6d8e83f65dce04df81b550ef9d0f3521b812057d
Ali <ayesha.ali19@mbpr1315-1786.trinity.nyc.ny.us>
2018-06-01 14:18:49 -0400

    message

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,25 +1,71 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
 
-public class Control : MonoBehaviour
-{
-    public void Multiplayer()
-    {
+public class Control : MonoBehaviour {
+    private List <GameObject> rootObjects = new List<GameObject>();
+    private GameObject gameObject;
+
+    public void Multiplayer() {
         SceneManager.LoadScene("multiplayer");
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(rootObjects);
+
+        for (int i = 0; i < rootObjects.Count; ++i)
+        {
+            gameObject = rootObjects[i];
+            gameObject.SetActive(true);
+        }
     }
 
-    public void Infinite()
-    {
+    public void Infinite() {
         SceneManager.LoadScene("main_scene");
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(rootObjects);
+
+        for (int i = 0; i < rootObjects.Count; ++i)
+        {
+            gameObject = rootObjects[i];
+            gameObject.SetActive(true);
+        }
     }
 
-    public void Levels()
-    {
+    public void Levels() {
         SceneManager.LoadScene("levels");
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(rootObjects);
+
+        for (int i = 0; i < rootObjects.Count; ++i)
+        {
+            gameObject = rootObjects[i];
+            gameObject.SetActive(true);
+        }
     }
 
-    public void title()
-    {
+    public void title() {
         SceneManager.LoadScene("title_page");
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(rootObjects);
+
+        for (int i = 0; i < rootObjects.Count; ++i) {
+            gameObject = rootObjects[i];
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void dontChange() {
+        Scene scene = SceneManager.GetActiveScene();
+        Debug.Log("Active scene is '" + scene.name + "'.");
+        scene.GetRootGameObjects(rootObjects);
+
+        // iterate root objects and do something
+        for (int i = 0; i < rootObjects.Count; ++i){
+            if (gameObject.name != "Main Camera") {
+                gameObject = rootObjects[i];
+                DontDestroyOnLoad(gameObject);
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
